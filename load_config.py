d47e5cd23818854036067817c1aebff89ae5ed92
ashaba1in <ashabalin9999@gmail.com>
2021-05-20 00:25:39 +0300

    -

--- load_config.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-def load_config(path):
-    with open(path, 'r') as f:
-        return yaml.load(f, Loader=yaml.FullLoader)
-
-config = load_config('/content/drive/MyDrive/Diploma/configs/rl_config.yaml')
--- metrics.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def mmr(ranks):
-    return np.mean(1 / np.array(ranks))
-
-def hit_k(ranks, k=10):
-    return np.mean(np.array(ranks) <= k)
